e3af7ecb8ce5cbd2fdf217690183225773b8d452
e1ysium <43900243+deltaRing@users.noreply.github.com>
2022-11-27 15:21:58 +0800

    Update D_Huffman.cpp
    加入编码器 解码器部分

--- D_Huffman.cpp
+++ D_Huffman.cpp
@@ -1,22 +1,25 @@
+// 算术编码器 1 动态哈夫曼编码
 #include "BitTree.h"
 #include <iostream>
 
 int main()
 {
-	std::string data = "ABBAABBCCCBAADDDDDAABBCCCBBDDDDDD";
+	std::string data = "lsiahifbdifishdsijfhdsilfhaoifgbodaifhgadfidsiufhdidsjiosjdsdjasdasdsadasdsadasfdsfdadafdasfasf";
 	_bit_tree_ * _header_ = _init_header_();
+	_bit_tree_ * de_header_ = _init_header_();
 
 	for (int ii = 0; ii < data.size(); ii++) {
+		init_transmit_byte();
 		bool _res_ = _find_nodes_(_header_, data.at(ii));
 		if (!_res_) {
 			_find_nodes_(_header_, data.at(ii), true);
 		}
+		_dhuff_encode_(_header_, data[ii], _res_);
 		_update_bit_tree_(_header_);
-		int label = 0;
-		_label_nodes_(_header_, label);
 	}
+	_dhuff_decode_(de_header_, transmit_buff);
 
-
-	std::cout << "Hello World!\n";
+	_remove_bit_tree_(_header_);
+	_remove_bit_tree_(de_header_);
 	return 0;
 }
